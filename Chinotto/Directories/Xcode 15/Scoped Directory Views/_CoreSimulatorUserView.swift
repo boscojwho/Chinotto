b65d31b51ccedb70a722f492ff17756e351904fd
Bosco Ho <boscojwho@gmail.com>
2023-11-21 20:22:55 -0800

    - Remove unused features.

--- Chinotto/Directories/Xcode 15/Scoped Directory Views/_CoreSimulatorUserView.swift
+++ Chinotto/Directories/Xcode 15/Scoped Directory Views/_CoreSimulatorUserView.swift
@@ -13,12 +13,6 @@ struct _CoreSimulatorUserView: View {
     
     @State private var storageViewModel: StorageViewModel = .init(directory: .coreSimulator)
     
-    @State private var showInspectorView = false
-    @State private var inspectorData: CoreSimulator_User?
-    
-    @State private var selectedFiles: Set<SizeMetadata.ID> = .init()
-    @State private var selectedDirs: Set<SizeMetadata.ID> = .init()
-    
     var body: some View {
         VSplitView {
             List {
@@ -62,71 +56,6 @@ struct _CoreSimulatorUserView: View {
                 }
             }
             .listStyle(.inset)
-        }
-    }
-    
-    @ViewBuilder
-    private func metadataTableView() -> some View {
-        Table(
-            storageViewModel.fileSizeMetadata,
-            selection: $selectedFiles
-        ) {
-            TableColumn("Size", value: \.value)
-            TableColumn("File", value: \.key.lastPathComponent)
-        }
-        .contextMenu {
-            Button {
-                showFilesInFinder()
-            } label: {
-                Text("Show in Finder")
-            }
-            .disabled(selectedFiles.isEmpty)
-        }
-        
-        Table(
-            storageViewModel.dirSizeMetadata,
-            selection: $selectedDirs
-        ) {
-            TableColumn("Size", value: \.value)
-            TableColumn("Directory", value: \.key.lastPathComponent)
-        }
-        .contextMenu {
-            Button {
-                showDirsInFinder()
-            } label: {
-                Text("Show in Finder")
-            }
-            .disabled(selectedDirs.isEmpty)
-        }
-    }
-    
-    private func showFilesInFinder() {
-        let filePaths = selectedFiles
-        let fileUrls = filePaths.compactMap { URL(string: $0 ) }
-        NSWorkspace.shared.activateFileViewerSelecting(fileUrls)
-    }
-    
-    private func showDirsInFinder() {
-        let dirPaths = selectedDirs
-        let dirUrls = dirPaths.compactMap { URL(string: $0 ) }
-        NSWorkspace.shared.activateFileViewerSelecting(dirUrls)
-    }
-    
-    @ViewBuilder
-    private func makeInspectorView() -> some View {
-        if let inspectorData {
-            switch inspectorData {
-            case .caches:
-                Text("")
-            case .devices:
-                @Bindable var vm = storageViewModel
-                _CoreSimulatorDevicesView(dirScope: .user, storageViewModel: $vm)
-                    .inspectorColumnWidth(min: 320, ideal: 600)
-            case .temp:
-                Text("")
-            }
-        } else {
-            Text("Select directory data.")
         }
     }
 }
