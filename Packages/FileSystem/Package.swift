ae244ad59ff0c5af0a95b303d063460b99d19914
Bosco Ho <boscojwho@gmail.com>
2023-11-30 17:40:16 -0800

    ^ Fix crash when calling SwiftUI observable on non-main thread. + Add async calculateSize() ~ Set platform version for FileSystem package.

--- Packages/FileSystem/Package.swift
+++ Packages/FileSystem/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FileSystem",
+    platforms: [.macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
